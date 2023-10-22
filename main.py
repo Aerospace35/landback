d77d44c64df320d6b1b306da9b6f8b30dec5d147
Brodie Friesen <76886939+Aerospace35@users.noreply.github.com>
2023-10-21 18:13:31 -0600

    Update main.py to new defaults

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import zipfile
 import random
 import os
 import csv
+import string
+import random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
 
 class config:
     def __init__(self):
